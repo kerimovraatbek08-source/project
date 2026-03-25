60cb9f4e2aff00b87678abb1ce4edd74b267ef88
kerimovraatbek08-source <kerimovraatbek08@gmail.com>
2026-03-25 20:11:33 +0600

    Delete project/project-2 directory

--- project/project-2/app.js
+++
@@ -1,9 +0,0 @@
-function showPage(id) {
-  const pages = document.querySelectorAll(".page");
-
-  pages.forEach((page) => {
-    page.style.display = "none";
-  });
-
-  document.getElementById(id).style.display = "block";
-}
